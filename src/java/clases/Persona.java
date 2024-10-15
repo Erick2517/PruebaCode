d0533e73b2b4aaf287cd3458468090840c2f6da0
Erick2517 <ericklandaverde15@gmail.com>
2024-10-14 22:05:05 -0600

    Commit para iniciar el proyecto

---
+++ src/java/clases/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author erick
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+
+    public Persona(){}
+
+    public Persona(String pNombre, String pApellido){
+        this.nombre = pNombre;
+        this.apellido = pApellido;
+    }
+
+    
+}
